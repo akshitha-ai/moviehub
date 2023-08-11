49ac448a3710abe82c243a4ce610d9e148e7d9b1
Akshitha Ravichandran <ravichandranakshitha@gmail.com>
2023-08-11 19:17:56 +0530

    new

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+
+
+
 
 const Trending = () => {
+
+  const [content, setContent] = useState([]);
+  
+  const fetchTrending =async () => {
+    const { data } = await axios.get(`https://api.themoviedb.org/3/trending/all/day?language=en-US&api_key=${process.env.REACT_APP_API_KEY}`);
+
+    
+    console.log(data);
+    setContent(data.results);
+  };
+
+  useEffect(() => {
+    fetchTrending();
+  }, []);
   return (
     <div>
         <span className='pageTrend'>Trending</span>
     </div>
-  )
-}
+  );
+};
 
 export default Trending
